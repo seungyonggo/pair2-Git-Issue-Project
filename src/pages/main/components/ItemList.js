c2fd6c33936a7a241932b5210211a71d698a1b24
seungyonggo <tmddyd432@gmail>
2023-06-14 16:48:34 +0900

    feat: 페이지네이션 기능구현 완료 필터를 사용하는걸 생각하지 못하고 개발해서 주석으로 남김

--- src/pages/main/components/ItemList.js
+++ src/pages/main/components/ItemList.js
@@ -27,3 +27,54 @@ const Wrapper = styled.div`
 const S = {
 	Wrapper,
 }
+
+// import React from 'react'
+// import OneItem from './OneItem'
+// import styled from 'styled-components'
+
+// const ItemList = ({ data, limit, page }) => {
+// 	console.log(data)
+// 	const startIndex = (page - 1) * limit
+// 	const endIndex = startIndex + limit
+// 	const slicedData = data.slice(startIndex, endIndex)
+
+// 	return (
+// 		<Wrapper>
+// 			{data.length > 0 ? (
+// 				slicedData.map(item => (
+// 					<OneItem key={Math.floor(Math.random() * 1000000)} item={item} />
+// 				))
+// 			) : (
+// 				<div>No items to display</div>
+// 			)}
+// 		</Wrapper>
+// 	)
+// }
+
+// const Wrapper = styled.div`
+// 	margin: 0 auto;
+// 	width: 80%;
+// 	display: flex;
+// 	flex-direction: column;
+// 	justify-content: center;
+// 	align-items: center;
+// `
+
+// // const S = {
+// // 	Wrapper,
+// // }
+
+// export default ItemList
+// const ItemList = ({ data, limit, page }) => {
+// 	console.log(data)
+// 	const startIndex = (page - 1) * limit
+// 	const endIndex = startIndex + limit
+// 	const slicedData = data.slice(startIndex, endIndex)
+// 	return (
+// 		<S.Wrapper>
+// 			{slicedData.map(item => {
+// 				return <OneItem key={Math.floor(Math.random() * 1000000)} item={item} />
+// 			})}
+// 		</S.Wrapper>
+// 	) // number , title, update_at, user.login,
+// }
--- src/reducer/issue.js
+++ src/reducer/issue.js
@@ -60,3 +60,107 @@ export const getIssue = createAsyncThunk(
 		}
 	},
 )
+// import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+// import { mainApi } from '../apis/MainApi'
+
+// const initialState = {
+// 	issue: [],
+// 	getIssueState: {
+// 		loading: false,
+// 		done: false,
+// 		err: null,
+// 	},
+// }
+
+// export const issueSlice = createSlice({
+// 	name: 'issue',
+// 	initialState,
+// 	extraReducers: builder => {
+// 		builder.addCase(getIssue.pending, state => {
+// 			state.getIssueState.loading = true
+// 			state.getIssueState.done = false
+// 			state.getIssueState.err = null
+// 		})
+// 		builder.addCase(getIssue.fulfilled, (state, action) => {
+// 			state.getIssueState.loading = false
+// 			state.getIssueState.done = true
+// 			state.getIssueState.err = null
+// 			state.issue = action.payload // load data
+// 		})
+// 		builder.addCase(getIssue.rejected, (state, action) => {
+// 			state.getIssueState.loading = false
+// 			state.getIssueState.done = true
+// 			state.getIssueState.err = action.payload
+// 		})
+// 	},
+// })
+// export const getIssue = createAsyncThunk(
+// 	'issue/getIssue',
+// 	async ({ page, limit }, thunkAPI) => {
+// 		try {
+// 			const response = await mainApi.getApi(
+// 				`/repos/angular/angular-cli/issues?page=${page}&per_page=${limit}`,
+// 			)
+// 			return response.data
+// 		} catch (error) {
+// 			return thunkAPI.rejectWithValue(error.message)
+// 		}
+// 	},
+// )
+
+//api에서 받아온 issue 데이터를 관리하는 state
+//issue
+// const initialState = {
+// 	issue: [], //axios로 데이터가 담김
+// 	getIssueState: {
+// 		//pending, fullfilled, rejected
+// 		loading: false, //T  F  F ==> true => 로딩 페이지를 보여줌(true)
+// 		done: false, //F T T
+// 		err: null, //F F 에러메세지
+// 	},
+// }
+//extraReducer를 사용해 getIssue가 실행되고, axios 상태에 따라
+//getIssueState안의 loading, done, err를 바꿔줌
+// export const issueSlice = createSlice({
+// 	name: 'issue',
+// 	initialState,
+// 	extraReducers: builder => {
+// 		builder.addCase(getIssue.pending, state => {
+// 			state.getIssueState.loading = true
+// 			state.getIssueState.done = false
+// 			state.getIssueState.err = null
+// 		})
+// 		builder.addCase(getIssue.fulfilled, (state, action) => {
+// 			state.getIssueState.loading = false
+// 			state.getIssueState.done = true
+// 			state.getIssueState.err = null
+// 			//성공을 했을 때 => issue데이터가 잘 들어옴 => issue데이터를 넣어줘야함 => usestate x
+// 			state.issue = action.payload //데이터 담음
+// 		})
+// 		builder.addCase(getIssue.rejected, (state, action) => {
+// 			state.getIssueState.loading = false
+// 			state.getIssueState.done = true
+// 			state.getIssueState.err = action.payload
+// 		})
+// 	},
+// })
+//실행될 getIssue 함수
+//createAsyncThunk : 첫번째 인자로 준 issue/getIssue에 따라 각각 pending,fulfilled,rejected
+//액션 상태를 자동으로 생성한다.
+//1. issue/getIssue/pending
+//2.issue/getIssue/..
+//3.issue/getIssue/...
+// export const getIssue = createAsyncThunk(
+// 	'issue/getIssue',
+// 	async (page, issue) => {
+// 		try {
+// 			const res = await mainApi.getApi(page, issue)
+// 			console.log('getApi', res.data)
+// 			console.log('res', res)
+// 			return res.data
+// 		} catch (err) {
+// 			console.log(err)
+// 			return err
+// 		}
+// 	},
+// )
