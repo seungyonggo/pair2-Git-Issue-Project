d88611353bca36772f0959c10355e8b52ff5ae2e
ooherin <555ohr@naver.com>
2023-06-14 09:51:37 +0900

    feat: rtk 수정;

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import './App.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './routes/routing'
 import { Provider } from 'react-redux'
-import { store } from './store/Store'
+import { store } from './store/store'
+
 function App() {
 	return (
 		<Provider store={store}>
--- src/store/Store.js
+++ src/store/Store.js
@@ -1,13 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { rootReducer } from '../reducer'
+import logger from 'redux-logger'
 
 export const store = configureStore({
-	reducer: rootReducer,
+	reducer: rootReducer, //상위의 리듀서 => 작은 slice를 만들어서 집어넣음
 	devtools: process.env.NODE_ENV === 'development',
-	// middleware: defaultMiddleware => {
-	// 	if (process.env.NODE_ENV === 'development') {
-	// 		return [...defaultMiddleware(), logger]
-	// 	}
-	// 	return defaultMiddleware
-	// },
+	middleware: defaultMiddleware => {
+		if (process.env.NODE_ENV === 'development') {
+			return [...defaultMiddleware(), logger] //redux-logger
+		}
+		return defaultMiddleware
+	},
 })
